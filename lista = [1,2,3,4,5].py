a123d353ec650b6a76b11b9040f5223b156078e3
fran653 <fran653@gmail.com>
2025-04-15 10:52:14 +0200

    en el nuevo pc!

---
+++ lista = [1,2,3,4,5].py
@@ -0,0 +1,10 @@
+lista = [1,2,3,4,5]
+resultado = sum(lista, 20)
+iterador = iter(lista)
+elemento = next(iterador)
+print(f'Siguiente elemento del iterable: {next(iterador)}')
+print(f'Siguiente elemento del iterable: {next(iterador)}')
+print(f'Siguiente elemento del iterable: {next(iterador)}')
+print(f'Siguiente elemento del iterable: {elemento}')
+print(f'Siguiente elemento del iterable: {elemento}')
+import numpy as np
